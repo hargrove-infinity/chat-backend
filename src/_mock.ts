181271cd420d2917b709421b6b07df23b25188f3
hargrove <hargrove.infinity@gmail.com>
2026-01-21 20:41:48 +0200

    added mocked database and login endpoint

---
+++ src/_mock.ts
@@ -0,0 +1,47 @@
+export const mockedUsers = [
+  {
+    password: "Coffee92",
+    email: "michael.thompson@mail.com",
+    firstName: "Michael",
+    lastName: "Thompson",
+    isAdmin: true,
+    token:
+      "eyJlbWFpbCI6Im1pY2hhZWwudGhvbXBzb25AbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJNaWNoYWVsIiwibGFzdE5hbWUiOiJUaG9tcHNvbiIsImlzQWRtaW4iOnRydWV9",
+  },
+  {
+    password: "Sunrise24",
+    email: "james.walker@mail.com",
+    firstName: "James",
+    lastName: "Walker",
+    isAdmin: false,
+    token:
+      "eyJlbWFpbCI6ImphbWVzLndhbGtlckBtYWlsLmNvbSIsImZpcnN0TmFtZSI6IkphbWVzIiwibGFzdE5hbWUiOiJXYWxrZXIiLCJpc0FkbWluIjpmYWxzZX0=",
+  },
+  {
+    password: "BlueSky07",
+    email: "daniel.harris@mail.com",
+    firstName: "Daniel",
+    lastName: "Harris",
+    isAdmin: false,
+    token:
+      "eyJlbWFpbCI6ImRhbmllbC5oYXJyaXNAbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJEYW5pZWwiLCJsYXN0TmFtZSI6IkhhcnJpcyIsImlzQWRtaW4iOmZhbHNlfQ==",
+  },
+  {
+    password: "NightMoon15",
+    email: "ryan.mitchell@mail.com",
+    firstName: "Ryan",
+    lastName: "Mitchell",
+    isAdmin: false,
+    token:
+      "eyJlbWFpbCI6InJ5YW4ubWl0Y2hlbGxAbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJSeWFuIiwibGFzdE5hbWUiOiJNaXRjaGVsbCIsImlzQWRtaW4iOmZhbHNlfQ==",
+  },
+  {
+    password: "River88",
+    email: "christopher.reynolds@mail.com",
+    firstName: "Christopher",
+    lastName: "Reynolds",
+    isAdmin: false,
+    token:
+      "eyJlbWFpbCI6ImNocmlzdG9waGVyLnJleW5vbGRzQG1haWwuY29tIiwiZmlyc3ROYW1lIjoiQ2hyaXN0b3BoZXIiLCJsYXN0TmFtZSI6IlJleW5vbGRzIiwiaXNBZG1pbiI6ZmFsc2V9",
+  },
+];
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,30 @@
+import { createServer } from "node:http";
+
 import express from "express";
 import cors from "cors";
-import { createServer } from "node:http";
 import { Server } from "socket.io";
+
 import { envVariables } from "./common";
+import { mockedUsers } from "./_mock";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const foundUser = mockedUsers.find(
+    (user) => user.email === email && user.password === password,
+  );
+
+  if (foundUser) {
+    res.send({ payload: foundUser.token });
+    return;
+  }
+
+  res.status(400).send({ errors: ["Wrong credentials"] });
+});
 
 const server = createServer(app);
 
@@ -16,15 +35,89 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+const chatNamespace = io.of("/chat");
+
+const adminNamespace = io.of("/admin");
+
+chatNamespace.use((socket, next) => {
+  if (!socket.handshake.auth.token) {
+    console.log("User without token has been rejected");
+    return next(new Error("Missing token"));
+  }
+
+  console.log("User with token has been accepted");
+  next();
+});
+
+adminNamespace.use((socket, next) => {
+  if (!socket.handshake.auth.isAdmin) {
+    console.log("Non admin has been rejected");
+    return next(new Error("Missing admin role"));
+  }
+
+  if (!socket.handshake.auth.token) {
+    console.log("Admin without token has been rejected");
+    return next(new Error("Missing admin token"));
+  }
+
+  console.log("Admin with token has been accepted");
+  next();
+});
+
+adminNamespace.on("connection", (socket) => {
+  console.log("an admin user connected");
+
+  socket.on("disconnecting", (reason) => {
+    console.log("Reason of a disconnecting admin:", reason);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log("Reason of a disconnect admin:", reason);
+  });
+
+  // TODO: add disconnection later
+  // Disconnect for socket
+  // setTimeout(() => {
+  //   // true closes the underlying connection
+  //   socket.disconnect(true);
+  // }, 2000);
+
+  socket.emit("chat message", "Hello from the Backend admin namespace");
+});
+
+chatNamespace.on("connection", (socket) => {
+  console.log("a chat user connected");
 
   socket.on("chat message", (msg) => {
     console.log(`message: ${msg}`);
   });
 
-  socket.emit("chat message", "Hello from the Backend");
+  socket.on("disconnecting", (reason) => {
+    console.log("Reason of a disconnecting chat:", reason);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log("Reason of a disconnect chat:", reason);
+  });
+
+  // TODO: add disconnection later
+  // Disconnect for socket
+  // setTimeout(() => {
+  //   // true closes the underlying connection
+  //   socket.disconnect(true);
+  // }, 2000);
+
+  socket.emit("chat message", "Hello from the Backend chat namespace");
+
+  // TODO: apply this later
+  // if (chatNamespace.sockets.size > 1) {
+  //   chatNamespace.disconnectSockets();
+  // }
 });
+
+setInterval(() => {
+  adminNamespace.emit("metrics", { userCount: chatNamespace.sockets.size });
+}, 3000);
 
 server.listen(envVariables.port, () => {
   console.log("Server is running");
