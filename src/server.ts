4dc3edac18058f97ffc742a21b37e4af841ac8a4
hargrove <hargrove.infinity@gmail.com>
2026-03-08 15:10:57 +0200

    renamed files

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";
 
 import { createApp } from "./app";
-import { envVariables } from "./common/envVariables";
+import { envVariables } from "./common/env.config";
 import { initSockets } from "./sockets";
 
 const app = createApp();
