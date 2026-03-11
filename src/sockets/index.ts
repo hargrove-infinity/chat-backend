104b7df859a5473089f450d2a9c6529e22cfabe4
hargrove <hargrove.infinity@gmail.com>
2026-03-11 19:19:12 +0200

    stored log error on io.engine.on('connection_error')

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -2,8 +2,10 @@ import type { Server } from "socket.io";
 import { startAdminMetrics } from "../metrics/admin.metrics";
 import { initAdminNamespace } from "./admin/admin.namespace";
 import { initChatNamespace } from "./chat/chat.namespace";
+import { initGlobalHandlers } from "./global/global.namespace";
 
 export function initSockets(io: Server) {
+  initGlobalHandlers(io);
   const adminNamespace = initAdminNamespace(io);
   const chatNamespace = initChatNamespace(io);
   startAdminMetrics(adminNamespace, chatNamespace);
