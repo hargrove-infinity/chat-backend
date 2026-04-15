39813c2a41d080defd14b9506da1f47ad5989ccb
hargrove <hargrove.infinity@gmail.com>
2026-04-15 19:49:14 +0300

    added usage of user repository

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,11 @@
+// TODO: uncomment
+// import { eq } from "drizzle-orm";
 import { Router } from "express";
 import { db } from "../_mock/db";
 import { paths } from "../common/paths";
+// TODO: uncomment
+// import { userTable } from "../db/schema";
+// import { userRepository } from "../repositories/user.repository";
 
 export const authRoutes = Router();
 
@@ -11,16 +16,38 @@ export const authRoutes = Router();
 authRoutes.post(paths.auth.login, async (req, res) => {
   const { email, password } = req.body;
 
-  const foundUser = db.users.find(
+  const user = db.users.find(
     (user) => user.email === email && user.password === password,
   );
 
-  if (!foundUser) {
+  if (!user) {
     res.status(400).send({ errors: ["Wrong credentials"] });
     return;
   }
 
-  const { password: _, ...rest } = foundUser;
+  // TODO: uncomment
+  // const [user, error] = await userRepository.findFirst({
+  //   where: eq(userTable.email, email),
+  // });
+
+  // if (error) {
+  //   res.status(500).send({ errors: ["Internal server error"] });
+  //   return;
+  // }
+
+  // if (!user) {
+  //   res.status(400).send({ errors: ["Wrong credentials"] });
+  //   return;
+  // }
+
+  // // TODO: replace with bcrypt method
+  // if (user.password !== password) {
+  //   res.status(400).send({ errors: ["Wrong credentials"] });
+  //   return;
+  // }
+
+  const { password: _, ...rest } = user;
+
   const encoded = Buffer.from(JSON.stringify(rest)).toString("base64");
 
   res.send({ payload: encoded });
