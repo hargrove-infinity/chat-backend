7ac8794a6d6e76daf1e1c614e2c627c975ddf631
hargrove <hargrove.infinity@gmail.com>
2026-04-20 19:05:05 +0300

    renamed 'chatParticipantsExtended' into 'rawChats'

--- src/repositories/chat.repository.ts
+++ src/repositories/chat.repository.ts
@@ -8,40 +8,37 @@ import {
 } from "../db/schema";
 
 async function findManyByUserId(userId: string) {
-  const chatParticipantsExtended =
-    await db.query.chatParticipantsTable.findMany({
-      where: eq(chatParticipantsTable.userId, userId),
-      columns: { userId: false, chatId: false },
-      with: {
-        user: {
-          columns: { id: true },
-        },
-        chat: {
-          columns: { id: true, name: true, type: true },
-          with: {
-            messages: {
-              where: eq(messageTable.chatId, chatParticipantsTable.chatId),
-              orderBy: [desc(messageTable.createdAt)],
-              columns: { content: true },
-              limit: 1,
-            },
-            chatParticipants: {
-              columns: { userId: false, chatId: false },
-              with: {
-                user: {
-                  columns: {
-                    id: true,
-                    firstName: true,
-                    lastName: true,
-                    socketId: true,
-                  },
+  const rawChats = await db.query.chatParticipantsTable.findMany({
+    where: eq(chatParticipantsTable.userId, userId),
+    columns: { userId: false, chatId: false },
+    with: {
+      user: { columns: { id: true } },
+      chat: {
+        columns: { id: true, name: true, type: true },
+        with: {
+          messages: {
+            where: eq(messageTable.chatId, chatParticipantsTable.chatId),
+            orderBy: [desc(messageTable.createdAt)],
+            columns: { content: true },
+            limit: 1,
+          },
+          chatParticipants: {
+            columns: { userId: false, chatId: false },
+            with: {
+              user: {
+                columns: {
+                  id: true,
+                  firstName: true,
+                  lastName: true,
+                  socketId: true,
                 },
               },
             },
           },
         },
       },
-    });
+    },
+  });
 
   const userUnreadEvents = await db.query.messageStatusTable.findMany({
     where: and(
@@ -62,47 +59,43 @@ async function findManyByUserId(userId: string) {
     unreadCountByChatId[chatId] = (unreadCountByChatId[chatId] ?? 0) + 1;
   }
 
-  const chatDtos: ChatDTO[] = chatParticipantsExtended.map(
-    (chatParticipant) => {
-      const participants = chatParticipant.chat.chatParticipants.map(
-        (participant) => {
-          return {
-            id: participant.user.id,
-            name: `${participant.user.firstName} ${participant.user.lastName}`,
-            isTyping: false,
-          };
-        },
+  const chatDtos: ChatDTO[] = rawChats.map((rawChat) => {
+    const participants = rawChat.chat.chatParticipants.map((participant) => {
+      return {
+        id: participant.user.id,
+        name: `${participant.user.firstName} ${participant.user.lastName}`,
+        isTyping: false,
+      };
+    });
+
+    const defaultChatData = {
+      id: rawChat.chat.id,
+      type: rawChat.chat.type,
+      name: rawChat.chat.name,
+      participants: participants,
+      lastMessage: rawChat.chat.messages[0]?.content ?? null,
+      isOnline: false,
+      unreadMessages: unreadCountByChatId[rawChat.chat.id] ?? 0,
+    };
+
+    if (rawChat.chat.type === "DIRECT") {
+      const interlocutor = rawChat.chat.chatParticipants.find(
+        (participant) => participant.user.id !== rawChat.user.id,
       );
 
-      const defaultChatData = {
-        id: chatParticipant.chat.id,
-        type: chatParticipant.chat.type,
-        name: chatParticipant.chat.name,
-        participants: participants,
-        lastMessage: chatParticipant.chat.messages[0]?.content ?? null,
-        isOnline: false,
-        unreadMessages: unreadCountByChatId[chatParticipant.chat.id] ?? 0,
-      };
-
-      if (chatParticipant.chat.type === "DIRECT") {
-        const interlocutor = chatParticipant.chat.chatParticipants.find(
-          (participant) => participant.user.id !== chatParticipant.user.id,
-        );
-
-        if (!interlocutor) {
-          throw new Error("Interlocutor was not found");
-        }
-
-        return {
-          ...defaultChatData,
-          name: `${interlocutor.user.firstName} ${interlocutor.user.lastName}`,
-          isOnline: !!interlocutor.user.socketId,
-        };
+      if (!interlocutor) {
+        throw new Error("Interlocutor was not found");
       }
 
-      return defaultChatData;
-    },
-  );
+      return {
+        ...defaultChatData,
+        name: `${interlocutor.user.firstName} ${interlocutor.user.lastName}`,
+        isOnline: !!interlocutor.user.socketId,
+      };
+    }
+
+    return defaultChatData;
+  });
 
   return chatDtos;
 }
