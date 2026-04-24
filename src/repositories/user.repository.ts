6099431815c0a146d38ae66b6f66590d6e75f16f
hargrove <hargrove.infinity@gmail.com>
2026-04-24 18:38:37 +0300

    added findAuthorSocketMessageGroups

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,6 @@
-import { eq } from "drizzle-orm";
+import { and, eq, inArray, isNotNull, sql } from "drizzle-orm";
 import { db } from "../db";
-import { userTable } from "../db/schema";
+import { messageTable, userTable } from "../db/schema";
 
 async function findFirstByEmail(email: string) {
   const user = await db.query.userTable.findFirst({
@@ -10,6 +10,23 @@ async function findFirstByEmail(email: string) {
   return user;
 }
 
+async function findAuthorSocketMessageGroups(messageIds: string[]) {
+  const data = await db
+    .select({
+      authorSocketId: userTable.socketId,
+      messageIds: sql<string[]>`array_agg(${messageTable.id})`,
+    })
+    .from(messageTable)
+    .innerJoin(userTable, eq(userTable.id, messageTable.userId))
+    .where(
+      and(inArray(messageTable.id, messageIds), isNotNull(userTable.socketId)),
+    )
+    .groupBy(userTable.socketId);
+
+  return data;
+}
+
 export const userRepository = {
   findFirstByEmail,
+  findAuthorSocketMessageGroups,
 } as const;
