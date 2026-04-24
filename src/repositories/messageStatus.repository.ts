b14a7787867762d5d97119eb844ad01cc595eb75
hargrove <hargrove.infinity@gmail.com>
2026-04-24 22:41:59 +0300

    added updateMessagesAsRead

---
+++ src/repositories/messageStatus.repository.ts
@@ -0,0 +1,24 @@
+import { and, eq, inArray } from "drizzle-orm";
+import { db } from "../db";
+import { messageStatusTable } from "../db/schema";
+import type { ReadReceiptPayload } from "../sockets/chat/chat.types";
+
+async function updateMessagesAsRead(payload: ReadReceiptPayload) {
+  const { messageIds, readerId } = payload;
+
+  const data = await db
+    .update(messageStatusTable)
+    .set({ read: false })
+    .where(
+      and(
+        inArray(messageStatusTable.messageId, messageIds),
+        eq(messageStatusTable.userId, readerId),
+      ),
+    );
+
+  return data;
+}
+
+export const messageStatusRepository = {
+  updateMessagesAsRead,
+} as const;
