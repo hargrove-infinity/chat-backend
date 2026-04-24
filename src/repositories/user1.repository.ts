f5d80e4e6eb483aacdec8d47c0085bce3e615529
hargrove <hargrove.infinity@gmail.com>
2026-04-24 17:03:48 +0300

    added findOnlineDirectInterlocutorsSocketIds

---
+++ src/repositories/user1.repository.ts
@@ -0,0 +1,50 @@
+import { and, eq, inArray, isNotNull, ne } from "drizzle-orm";
+import { db } from "../db";
+import { chatParticipantsTable, chatTable, userTable } from "../db/schema";
+
+async function findOnlineDirectInterlocutorsSocketIds(userId: string) {
+  const rawDirectChatIds = await db
+    .select({ chatId: chatTable.id })
+    .from(chatParticipantsTable)
+    .leftJoin(chatTable, eq(chatParticipantsTable.chatId, chatTable.id))
+    .where(
+      and(
+        eq(chatParticipantsTable.userId, userId),
+        eq(chatTable.type, "DIRECT"),
+      ),
+    );
+
+  const directChatIds = rawDirectChatIds.flatMap((item) => {
+    if (typeof item.chatId === "string" && item.chatId.length > 0) {
+      return item.chatId;
+    }
+    return [];
+  });
+
+  const rawUserDirectChatIds = await db.query.chatParticipantsTable.findMany({
+    where: and(
+      inArray(chatParticipantsTable.chatId, directChatIds),
+      ne(chatParticipantsTable.userId, userId),
+    ),
+  });
+
+  const userIds = rawUserDirectChatIds.map((item) => item.userId);
+
+  const rawUserSocketIds = await db.query.userTable.findMany({
+    where: and(inArray(userTable.id, userIds), isNotNull(userTable.socketId)),
+    columns: { socketId: true },
+  });
+
+  const userSocketIds = rawUserSocketIds.flatMap((item) => {
+    if (typeof item.socketId === "string" && item.socketId.length > 0) {
+      return item.socketId;
+    }
+    return [];
+  });
+
+  return userSocketIds;
+}
+
+export const userRepository = {
+  findOnlineDirectInterlocutorsSocketIds,
+} as const;
