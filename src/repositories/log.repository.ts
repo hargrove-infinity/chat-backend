c9b0db76301f8656060e8e4a52d85b9c76361bc3
hargrove <hargrove.infinity@gmail.com>
2026-04-30 18:53:12 +0300

    changed arguments from union logModel: NewLog | NewLog[] to logModels: NewLog[] in logRepository.create

--- src/repositories/log.repository.ts
+++ src/repositories/log.repository.ts
@@ -1,9 +1,8 @@
 import { db } from "../db";
 import { logTable, type NewLog } from "../db/schema";
 
-async function create(logModel: NewLog | NewLog[]) {
-  const values = Array.isArray(logModel) ? logModel : [logModel];
-  const createdLogs = await db.insert(logTable).values(values).returning();
+async function create(logModels: NewLog[]) {
+  const createdLogs = await db.insert(logTable).values(logModels).returning();
   return createdLogs;
 }
 
