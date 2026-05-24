3e03e5deb1748dd148a3ba978de9ad4f26b9910f
hargrove <hargrove.infinity@gmail.com>
2026-05-24 19:29:34 +0300

    applied z.url() for databaseUrl, redisUrl in envSchema

--- src/validation/env.ts
+++ src/validation/env.ts
@@ -2,9 +2,7 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   port: z.string().transform((val) => parseInt(val, 10)),
-  // TODO: remake validation so check url
-  databaseUrl: z.string().nonempty(),
-  // TODO: remake validation so check url
-  redisUrl: z.string().nonempty(),
+  databaseUrl: z.url(),
+  redisUrl: z.url(),
   frontendUrl: z.url(),
 });
