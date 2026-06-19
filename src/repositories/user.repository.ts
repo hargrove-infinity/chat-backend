22440c58bc652a38b3f4e4a54c71aede147a2764
hargrove <hargrove.infinity@gmail.com>
2026-06-19 15:50:15 +0300

    adjusted columns in users table

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -23,6 +23,7 @@ async function updateBy(args: UserUpdateByArgs) {
   return user;
 }
 
+// TODO: Maybe move this into chatParticipantsRepository?
 async function findUserIdsDirectChats({
   directChatIds,
   userId,
